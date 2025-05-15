4ff53661f07eb74c01ef0f4b015bf8140da383fb
melanieokeeffee <92605803+melanieokeeffee@users.noreply.github.com>
2025-05-15 22:08:16 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Добро пожаловать в SmartCook!</h1>
+      <p>Это ваше приложение для управления рецептами.</p>
+    </div>
+  );
+}
+
+export default App;
