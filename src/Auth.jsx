2d2900b721fd64479d0e4bd1b5c13e0b39df7c5b
melanieokeeffee <92605803+melanieokeeffee@users.noreply.github.com>
2025-05-15 22:29:00 +0300

    Update Auth.jsx

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://zudndwhrynoqkviubbtp.supabase.co';
-const supabaseAnonKey = 'ТВОЙ_АНОНИМНЫЙ_КЛЮЧ'; // вставь сюда свой anon ключ из Supabase
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1ZG5kd2hyeW5vcWt2aXViYnRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMDI3NzAsImV4cCI6MjA2Mjg3ODc3MH0.bb77J7MtJOUuqcMP1pt3u6ldvVVXBG500Ve7_kOpJ2Q'; // вставь сюда свой anon ключ из Supabase
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
